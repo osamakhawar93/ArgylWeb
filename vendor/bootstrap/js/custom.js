99c3fc994b9ea15980ea21431d0e49c93bece5a7
Osama Khawar <osamakhawar93@gmail.com>
2017-12-11 21:14:43 +0500

    asdasda

--- vendor/bootstrap/js/custom.js
+++ vendor/bootstrap/js/custom.js
@@ -9,6 +9,18 @@ $(document).ready(function () {
         itemsDesktop: [1199, 1],
         itemsDesktopSmall: [979, 1]
     });
+
+
+    $(".scroller").click(function () {
+        $('html,body').animate({
+            scrollTop: $(".margin-110").offset().top
+        },
+            'slow');
+    });
+    
+    
+ 
+
 });
 
 
@@ -22,31 +34,51 @@ window.onscroll = function (ev) {
     }
 
 
-    $(".scroller").click(function () {
-        $('html,body').animate({
-            scrollTop: $(".margin-110").offset().top
-        },
-            'slow');
-    });
 
 };
 
 
 
-
-var $root = $('html, body');
-
-$('a[href^="#"]').click(function () {
-    $root.animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500);
-
-    return false;
+// Select all links with hashes
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(":focus")) { // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          $target.focus(); // Set focus again
+        };
+      });
+    }
+  }
 });
 
 
 
-$(document).scroll(function () {
+/* $(document).scroll(function () {
     var y = $(this).scrollTop();
     var x = $("#roadmap").position();
 
@@ -56,7 +88,7 @@ $(document).scroll(function () {
     else {
         removeAnimations();
     }
-});
+}); */
 
 
 
